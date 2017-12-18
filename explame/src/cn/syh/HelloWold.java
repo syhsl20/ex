90c193c8a56c6857ed9678ce7b1c6d76a5c1e1c0
songyanhui <syhfd20@126.com>
2017-12-18 15:54:04 +0800

    diyi

--- explame/src/cn/syh/HelloWold.java
+++ explame/src/cn/syh/HelloWold.java
@@ -8,5 +8,7 @@ public class HelloWold {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
     }
 }
